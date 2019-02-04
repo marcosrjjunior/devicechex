7fa7268501e9d2a4baf8f34b80d019e5570f4783
marcosrjjunior <marcos@april9.com.au>
2019-02-04 16:26:35 +1000

    Adding new x's

--- src/components/CheckerStore.js
+++ src/components/CheckerStore.js
@@ -7,26 +7,56 @@ class CheckerStore {
             name: "Device S 320px (Iphone 5/SE)",
             width: 320,
             height: 405
-        },
-        {
+        }, {
             name: "Device M 375px (Iphone 6/7/8)",
             width: 375,
             height: 405
-        },
-        {
+        }, {
             name: "Device L 425px",
-            width: 425,
-            height: 405
+            width: 405,
+            height: 425
         }, {
             name: "Iphone 6/7/8 Plus",
             width: 414,
             height: 736
+        }, {
+            name: "Pixel 2",
+            width: 411,
+            height: 731
         }, {
             name: "Ipad Pro",
             width: 768,
             height: 1024
         }, {
             name: "Tablet 768px",
+            width: 405,
+            height: 768
+        }, {
+            name: "Device S 320px (Iphone 5/SE) (Landscape)",
+            width: 405,
+            height: 320
+        }, {
+            name: "Device M 375px (Iphone 6/7/8) (Landscape)",
+            width: 405,
+            height: 375
+        }, {
+            name: "Device L 425px (Landscape)",
+            width: 425,
+            height: 405
+        }, {
+            name: "Iphone 6/7/8 Plus (Landscape)",
+            width: 736,
+            height: 414
+        }, {
+            name: "Pixel 2 (Landscape)",
+            width: 731,
+            height: 411
+        }, {
+            name: "Ipad Pro (Landscape)",
+            width: 1024,
+            height: 768
+        }, {
+            name: "Tablet 768px (Landscape)",
             width: 768,
             height: 405
         },
